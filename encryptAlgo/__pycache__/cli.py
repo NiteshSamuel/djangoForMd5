69d5fcf63063c61b217b4084dc64fada2b51c461
Nitesh Samuel <niteshjsamuel@gmail.com>
2021-06-13 21:38:10 +0530

    this file will work

---
+++ encryptAlgo/__pycache__/cli.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser
+from django.shorthands import render
+from md5 import MD5
+
+
+
+
+
+def main():
+    argument_parser = ArgumentParser(
+        description="Compute the MD5 hash of a given string.",
+    )
+    argument_parser.add_argument(
+        "string",
+        type=str,
+        help="The string to hash",
+    )
+
+    arguments = argument_parser.parse_args()
+    md5_hash = MD5.hash(arguments.string)
+
+    print(md5_hash)
+
+
+#if _name_ == "_main_":
+main()
